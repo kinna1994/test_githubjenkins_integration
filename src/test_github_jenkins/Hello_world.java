a72c9483e6cae8f144f09b5aa7c6d237f48be214
First Last <kiranpg94gmail@ip-172-31-66-198.ec2.internal>
2021-08-01 19:30:22 +0000

    first commit

---
+++ src/test_github_jenkins/Hello_world.java
@@ -0,0 +1,10 @@
+package test_github_jenkins;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world, I will be a devops engineer!!!");
+
+	}
+
+}
